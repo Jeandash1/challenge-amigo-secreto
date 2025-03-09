9e40f02935fca0f134eca3993785c27910fc26e2
Jendash1 <bernaljuan234@gmail.com>
2025-03-09 12:03:56 -0500

    crear array y muestre en lista los nombres

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Hola
+Este Proyecto es netamente educativo
+
+Resumen del proyecto:
+El proyecto consiste en ingresar varios nombres de amigos, al ingresar todos los amigos deseados puedes sortear el amigo al azar que se asignara a ti
+
--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function asignarTexto(elment, texto){
+    let elementoHTML = document.querySelector(elment);
+    elementoHTML.innerHTML = texto;
+}
+
+function limpiar(){
+    document.getElementById("amigo").value = "";
+}
+
+function agregarAmigo(){
+    
+    let nuevoAmigo = document.getElementById("amigo").value.trim();
+    if(nuevoAmigo != ""){
+        amigos.push(nuevoAmigo);
+        console.log(amigos)
+
+        let nuevoElement = document.createElement("li");
+        //document.getElementById("listaAmigos").innerHTML(amigos);
+        nuevoElement.textContent = nuevoAmigo
+        document.getElementById("listaAmigos").appendChild(nuevoElement);
+
+        limpiar();
+        
+    }else{
+        alert("Por favor, inserte un nombre.");
+    }
+}
