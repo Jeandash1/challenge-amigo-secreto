2ce44743b6270181509b1a3939d0d0754138bef1
Jeandash1 <bernaljuan234@gmail.com>
2025-03-10 23:59:17 -0500

    Se agrega el if para validar que hayan nombres en el arreglo

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-function asignarTexto(getElementById, texto){
-    let elementoHTML = document.querySelector(getElementById);
-    elementoHTML.innerHTML = texto;
-}
-
 function limpiar(){
     document.getElementById("amigo").value = "";
 }
@@ -30,14 +25,14 @@ function agregarAmigo(){
 }
 
 function sortearAmigo(){
-    let amigoRandom = Math.floor(Math.random() * amigos.length);
-    //console.log(`El nombre es ${amigos[amigoRandom]}`);    Validar que se esta escogiendo un nombre al azar 
-    
-    /*
-                ARREGLARLO !ESTA MAL
-    let amigoSeleccionado = amigos[amigoRandom]
-    document.getElementById("resultudo").textContent = `${amigoSeleccionado}`;
-            
-    */  
-    
+    if(amigos.length > 0){
+        document.getElementById("listaAmigos").innerHTML = "";
+        let amigoRandom = Math.floor(Math.random() * amigos.length);
+        //console.log(`El nombre es ${amigos[amigoRandom]}`);   // Validar que se esta escogiendo un nombre al azar 
+        
+        document.getElementById("resultado").innerHTML = `EL amigo secreto es: ${amigos[amigoRandom]}`;
+    }else{
+        alert("No hay amigos para sortear");
+    }
+
 }
